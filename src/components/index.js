2670afc526e6fe5f412b6912f411a0e46b299df8
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2024-02-25 13:33:48 +0530

    commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from './Footer/Footer.jsx'
+import Header from './Header/Header.jsx'
+import Container from './Container/Container.jsx'
+import Logo from './Logo/Logo.jsx'
+import LogoutBtn from './Header/LogoutBtn.jsx'
+import RTE from './RTE.jsx'
+export {Footer,Header,Container,Logo,LogoutBtn}
